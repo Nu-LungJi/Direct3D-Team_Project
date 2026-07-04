b5bda9829ba7aab78302024eff62f1b31b445d38
ForestMS1 <dasung56@gmail.com>
2026-07-04 19:24:48 +0900

    [FEAT] MapChunk시스템 구현

--- Engine/Private/MapManager.cpp
+++ Engine/Private/MapManager.cpp
@@ -1,9 +1,15 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "MapManager.h"
 #include "MapMeshObject.h"
 #include <fstream>
 
 NS_USING(Engine)
+
+#ifdef _DEBUG
+std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionColor>> CMapManager::_batch = nullptr;
+std::unique_ptr<DirectX::BasicEffect> CMapManager::_effect = nullptr;
+ComPtr<ID3D11InputLayout> CMapManager::_inputLayout = nullptr;
+#endif
 
 CMapManager::CMapManager()
 {
@@ -11,10 +17,47 @@ CMapManager::CMapManager()
 
 CMapManager::~CMapManager()
 {
+#ifdef _DEBUG
+	//-------DebugDraw-------
+	_batch.reset();
+	_effect.reset();
+	_inputLayout.Reset();
+	//-----------------------
+#endif
 }
 
 HRESULT CMapManager::Initialize()
 {
+
+#ifdef _DEBUG
+	// DebugDraw
+	// static 객체들이 아직 생성되지 않았다면 최초 1회 생성
+	if (_batch == nullptr)
+	{
+		// Batch 생성
+		_batch = std::make_unique<DirectX::PrimitiveBatch<DirectX::VertexPositionColor>>(CGameInstance::Get().GetGraphicDeviceContext().Get());
+
+		// Effect 생성 및 설정
+		_effect = std::make_unique<DirectX::BasicEffect>(CGameInstance::Get().GetGraphicDevice().Get());
+		_effect->SetVertexColorEnabled(true); // 정점 색상 사용 활성화
+
+		// InputLayout 생성 (BasicEffect의 셰이더 정보와 VertexPositionColor 구조를 연결)
+		void const* shaderByteCode;
+		size_t byteCodeLength;
+		_effect->GetVertexShaderBytecode(&shaderByteCode, &byteCodeLength);
+
+		HRESULT hr = CGameInstance::Get().GetGraphicDevice()->CreateInputLayout(
+			DirectX::VertexPositionColor::InputElements,
+			DirectX::VertexPositionColor::InputElementCount,
+			shaderByteCode,
+			byteCodeLength,
+			_inputLayout.GetAddressOf()
+		);
+
+		assert(SUCCEEDED(hr));
+	}
+#endif
+
 	return S_OK;
 }
 
@@ -31,13 +74,10 @@ void CMapManager::LateUpdate(_float fTimeDelta)
 
 }
 
-void CMapManager::UpdateGUI()
-{
-
-}
-
 HRESULT CMapManager::SaveMap(const std::string& path)
 {
+	RebuildChunks();
+
 	nlohmann::ordered_json rootJson = {};
 	rootJson["version"] = 1;
 	rootJson["objects"] = nlohmann::ordered_json::array();
@@ -60,6 +100,11 @@ HRESULT CMapManager::SaveMap(const std::string& path)
 			const auto& layerName = pair.first;
 
 			const auto& pTransform = pMeshObj->GetTransform();
+			const auto& pos = pTransform.GetPosition();
+			const auto& quat = pTransform.GetQuaternion();
+			const auto& scale = pTransform.GetScale();
+
+			MAPCHUNK_COORD coord = WorldToChunkCoord(pos);
 			
 			nlohmann::ordered_json objectJson =
 			{
@@ -71,22 +116,28 @@ HRESULT CMapManager::SaveMap(const std::string& path)
 				{"model", modelResourceTag},
 				{"layer", layerName},
 				{"position", {
-					pTransform.GetPosition().x,
-					pTransform.GetPosition().y,
-					pTransform.GetPosition().z
+					pos.x,
+					pos.y,
+					pos.z
 				}},
 				{"rotation", {
-					pTransform.GetQuaternion().x,
-					pTransform.GetQuaternion().y,
-					pTransform.GetQuaternion().z,
-					pTransform.GetQuaternion().w,
+					quat.x,
+					quat.y,
+					quat.z,
+					quat.w,
 
 				}},
 				{"scale", {
-					pTransform.GetScale().x,
-					pTransform.GetScale().y,
-					pTransform.GetScale().z
-				}}
+					scale.x,
+					scale.y,
+					scale.z
+				}},
+				{"chunk", {
+					{"x", coord.x},
+					{"y", coord.y},
+					{"z", coord.z}
+					}
+				}
 			};
 
 			rootJson["objects"].push_back(objectJson);
@@ -135,10 +186,9 @@ HRESULT CMapManager::LoadMap(const std::string& path, _bool clearBeforeLoad)
 			std::string model = objectJson["model"];
 			std::string layer = objectJson["layer"];
 
-			auto pos = objectJson["position"];
-			_float x = pos[0];
-			_float y = pos[1];
-			_float z = pos[2];
+			const auto& pos = objectJson["position"];
+			const auto& rot = objectJson["rotation"];
+			const auto& scale = objectJson["scale"];
 
 			E::CMapMeshObject::MAP_MESH_OBJECT_DESC Desc{};
 			Desc.sObjectTag = objectTag;
@@ -154,11 +204,127 @@ HRESULT CMapManager::LoadMap(const std::string& path, _bool clearBeforeLoad)
 				&Desc);
 
 			auto newObj = CGameInstance::Get().GetGameObjectByHandle(hObject.value());
-			newObj->GetTransform().SetPosition(XMVectorSet(x,y,z,1.f));
+			auto& newObjTransform = newObj->GetTransform();
+
+			newObjTransform.SetPosition(XMVectorSet(pos[0], pos[1], pos[2], 1.f));
+			newObjTransform.SetQuaternion(_float4{ rot[0], rot[1], rot[2], rot[3] });
+			newObjTransform.SetScale(_float3{ scale[0], scale[1], scale[2] });
+
+			//const auto& chunkJson = objectJson["chunk"];
+			// MAPCHUNK_COORD coord = {};
+			//coord.x = chunkJson["x"];
+			//coord.y = chunkJson["y"];
+			//coord.z = chunkJson["z"];
+
+			// json 수정이나 chunkSize 변경 시 달라질 수 있기 때문에 재 계산하여 로드
+			MAPCHUNK_COORD coord = WorldToChunkCoord({ pos[0], pos[1], pos[2] });
+			auto& chunk = m_Chunks[coord];
+			chunk.coord = coord;
+			chunk.hObjects.push_back(hObject.value());
+			chunk.bounds = MakeChunkBoundingBox(coord);
+			chunk.m_bDirty = true;
 		}
 	}
 	return S_OK;
 }
+
+_float3 CMapManager::GetChunkCenter(const MAPCHUNK_COORD& coord)
+{
+	return _float3(
+			{
+				(coord.x + 0.5f) * m_vChunkSize.x,
+				(coord.y + 0.5f) * m_vChunkSize.y,
+				(coord.z + 0.5f) * m_vChunkSize.z,
+			});
+}
+
+BoundingBox CMapManager::MakeChunkBoundingBox(const MAPCHUNK_COORD& coord)
+{
+	_float3 center = GetChunkCenter(coord);
+	_float3 extents = {
+		m_vChunkSize.x * 0.5f,
+		m_vChunkSize.y * 0.5f,
+		m_vChunkSize.z * 0.5f
+	};
+	return BoundingBox(center, extents);
+}
+
+MAPCHUNK_COORD CMapManager::WorldToChunkCoord(const _float3& pos) const
+{
+	return {
+		static_cast<int64_t>(std::floor(pos.x / m_vChunkSize.x)),
+		static_cast<int64_t>(std::floor(pos.y / m_vChunkSize.y)),
+		static_cast<int64_t>(std::floor(pos.z / m_vChunkSize.z))
+	};
+}
+
+void CMapManager::RebuildChunks()
+{
+	m_Chunks.clear();
+
+	const auto& layers = CGameInstance::Get().GetGameObjectLayers();
+
+	for (const auto& [layerName, layer] : layers)
+	{
+		for (const auto& handle : layer)
+		{
+			CGameObject* pObj = CGameInstance::Get().GetGameObjectByHandle(handle);
+
+			if (pObj == nullptr)
+				continue;
+
+			const auto& pos = pObj->GetTransform().GetPosition();
+
+			MAPCHUNK_COORD coord = WorldToChunkCoord(pos);
+
+			auto& chunk = m_Chunks[coord];
+			chunk.coord = coord;
+			chunk.hObjects.push_back(handle);
+			chunk.bounds = MakeChunkBoundingBox(coord);
+			chunk.m_bDirty = true;
+		}
+	}
+
+}
+
+#ifdef _DEBUG
+
+HRESULT CMapManager::RenderDebugMapChunk()
+{
+	if (m_bDebugDrawMapChunk == false)
+		return E_FAIL;
+
+	CCameraObject* cam = CGameInstance::Get().GetActiveCamera();
+
+	if (cam == nullptr)
+		return E_FAIL;
+
+	for (const auto& [coord, mapChunk] : m_Chunks)
+	{
+		_float3 center = GetChunkCenter(coord);
+		DrawBox(mapChunk.bounds, Colors::Lime, cam->GetView(), cam->GetProj(), XMMatrixIdentity());
+	}
+
+	return S_OK;
+}
+
+void CMapManager::DrawBox(const DirectX::BoundingBox& box, DirectX::FXMVECTOR color, DirectX::CXMMATRIX view, DirectX::CXMMATRIX projection, DirectX::CXMMATRIX world)
+{
+	_effect->SetView(view);
+	_effect->SetProjection(projection);
+	_effect->SetWorld(world);
+	CGameInstance::Get().GetGraphicDeviceContext()->IASetInputLayout(_inputLayout.Get());
+
+	// 그리기
+	_effect->Apply(CGameInstance::Get().GetGraphicDeviceContext().Get());
+	_batch->Begin();
+
+	// DirectXTK의 내장 Draw 함수 호출
+	DX::Draw(_batch.get(), box, color);
+
+	_batch->End();
+}
+#endif
 
 UPtr<CMapManager> CMapManager::Create()
 {
@@ -174,3 +340,4 @@ void CMapManager::Free()
 {
 	CEngineBase::Free();
 }
+
--- Engine/Public/MapManager.h
+++ Engine/Public/MapManager.h
@@ -1,7 +1,33 @@
 #pragma once
+
+#ifdef _DEBUG
+#include "DebugDraw.h"
+#include <directxtk/Effects.h>
+#endif
+
+#include <functional>
 #include "Engine_Base.h"
 
 NS_BEGIN(Engine)
+typedef struct tagMapChunk
+{
+	MAPCHUNK_COORD coord{};
+	std::vector<CHandle> hObjects{};
+	BoundingBox bounds{};
+	_bool m_bDirty = false;
+}MAPCHUNK;
+
+struct tagMapChunkCoordHash
+{
+	size_t operator()(const MAPCHUNK_COORD& coord) const
+	{
+		size_t h1 = std::hash<int64_t>{}(coord.x);
+		size_t h2 = std::hash<int64_t>{}(coord.y);
+		size_t h3 = std::hash<int64_t>{}(coord.z);
+
+		return h1 ^ (h2 << 1) ^ (h3 << 3);
+	}
+};
 
 class ENGINE_DLL CMapManager : public CEngineBase
 {
@@ -22,13 +48,44 @@ public:
 	void LateUpdate(_float fTimeDelta);
 
 public:
-	void UpdateGUI();
-
-public:
 	HRESULT SaveMap(const std::string& path);
 	HRESULT LoadMap(const std::string& path, _bool clearBeforeLoad = true);
 
+// ---------------------------------MapChunk-----------------------------------
+public:
+	void RebuildChunks();
+	const std::unordered_map<MAPCHUNK_COORD, MAPCHUNK, tagMapChunkCoordHash>& GetChunks() const { return m_Chunks; }
+	const _float3& GetChunkSize() const { return m_vChunkSize; }
 
+private:
+	_float3 GetChunkCenter(const MAPCHUNK_COORD& coord);
+	BoundingBox MakeChunkBoundingBox(const MAPCHUNK_COORD& coord);
+	MAPCHUNK_COORD WorldToChunkCoord(const _float3& pos) const;
+private:
+	_float3 m_vChunkSize = { 25.f, 25.f, 25.f };
+
+private:
+	std::unordered_map<MAPCHUNK_COORD, MAPCHUNK, tagMapChunkCoordHash> m_Chunks;
+
+
+// ---------------------------------MapChunk-----------------------------------
+
+#ifdef _DEBUG
+	// Debug Draw
+	// Debug Draw를 위한 도구들
+	// static으로 관리
+	static std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionColor>> _batch;
+	static std::unique_ptr<BasicEffect> _effect;
+	static ComPtr<ID3D11InputLayout> _inputLayout;
+public:
+	HRESULT RenderDebugMapChunk();
+	void SetDebugDrawMapChunk(_bool draw) { m_bDebugDrawMapChunk = draw; }
+private:
+	void DrawBox(const DirectX::BoundingBox& box, DirectX::FXMVECTOR color, DirectX::CXMMATRIX view, DirectX::CXMMATRIX projection, DirectX::CXMMATRIX world);
+
+private:
+	_bool m_bDebugDrawMapChunk = false;
+#endif
 //public:
 //	void FrameStart();
 //	void FrameEnd();
