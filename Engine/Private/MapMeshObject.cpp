65c6d48b2c757625e1a3ce03a65040e8b4473afb
ForestMS1 <dasung56@gmail.com>
2026-07-04 11:02:02 +0900

    [FEAT] MapMeshObject 틀 구현

---
+++ Engine/Private/MapMeshObject.cpp
@@ -0,0 +1,172 @@
+﻿#include "pch.h"
+#include "MapMeshObject.h"
+#include "ComConstantBuffer.h"
+#include "ComModelInstance.h"
+#include "GameInstance.h"
+#include "Resources.h"
+
+NS_USING(Engine)
+
+CMapMeshObject::CMapMeshObject()
+	: CGameObject{}
+{
+}
+
+CMapMeshObject::CMapMeshObject(const CMapMeshObject& Prototype)
+	: CGameObject{ Prototype }
+	, m_pResVertexShader{ Prototype.m_pResVertexShader }
+	, m_pResPixelShader{ Prototype.m_pResPixelShader }
+	, m_pResSamplerState{ Prototype.m_pResSamplerState }
+{
+}
+
+CMapMeshObject::~CMapMeshObject()
+{
+}
+
+HRESULT CMapMeshObject::InitializePrototype(void* pArg)
+{
+	m_pResVertexShader = CGameInstance::Get().GetResourceFirst<CResVertexShader>(TAG_RES_GRP_PERMANENT_SHADER, "VS_TestModelNonAnmi");
+	if (!m_pResVertexShader || FAILED(m_pResVertexShader->Load()))
+	{
+		return E_FAIL;
+	}
+
+	m_pResPixelShader = CGameInstance::Get().GetResourceFirst<CResPixelShader>(TAG_RES_GRP_PERMANENT_SHADER, "PS_TestModelNonAnmi");
+	if (!m_pResPixelShader || FAILED(m_pResPixelShader->Load()))
+	{
+		return E_FAIL;
+	}
+
+	m_pResSamplerState = CGameInstance::Get().GetResourceFirst<CResSamplerState>(TAG_RES_GRP_PERMANENT_STATE, TAG_RES_STATE_SS_LINEAR_WRAP);
+	if (!m_pResSamplerState)
+	{
+		return E_FAIL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CMapMeshObject::Initialize(void* pArg)
+{
+	if (FAILED(CGameObject::Initialize(pArg)))
+	{
+		return E_FAIL;
+	}
+
+	auto* pDesc = static_cast<MAP_MESH_OBJECT_DESC*>(pArg);
+	if (pDesc == nullptr)
+	{
+		return E_FAIL;
+	}
+
+	m_modelResourceGroup = pDesc->modelGroupTag;
+	m_modelResourceTag = pDesc->modelResTag;
+
+	{
+		CComConstantBuffer::DESC Desc{};
+		Desc.cBufferId = { TAG_RES_GRP_PERMANENT_BUFFER, TAG_RES_CBUFFER_OBJECT };
+		if (FAILED(AddComponentFromProto("PERMANENT", "Prototype_Component_ConstantBuffer", "ComCBufferPerObject", &Desc, &m_pComCBufferPerObject)))
+		{
+			return E_FAIL;
+		}
+	}
+
+	{
+		CComModelInstance::DESC Desc{};
+		Desc.sGroupTag = m_modelResourceGroup;
+		Desc.sResTag = m_modelResourceTag;
+		if (FAILED(AddComponentFromProto("PERMANENT", "Prototype_Component_ModelInstance", "ComCModelInstance", &Desc, &m_pComModelInstance)))
+		{
+			return E_FAIL;
+		}
+	}
+
+	return S_OK;
+}
+
+void CMapMeshObject::LateUpdate(_float fTimeDelta)
+{
+	GetTransform().Update();
+	CGameInstance::Get().AddRenderObject(RENDERGROUP::NONBLEND, this);
+}
+
+HRESULT CMapMeshObject::Render(ID3D11DeviceContext* pContext, const RENDER_CTX& ctx)
+{
+	if (m_pComModelInstance == nullptr || m_pComModelInstance->GetModel() == nullptr)
+	{
+		return E_FAIL;
+	}
+
+	{
+		CB_PER_OBJECT cbPerObject{};
+		cbPerObject.matWorld = *GetTransform().GetCombinedWorldMatrix();
+		XMStoreFloat4x4(&cbPerObject.matWVP, GetTransform().GetLoadedCombinedWorldMatrix() * ctx.matViewProj);
+		if (FAILED(m_pComCBufferPerObject->MapDiscard(pContext, &cbPerObject, sizeof(cbPerObject))))
+		{
+			return E_FAIL;
+		}
+		pContext->VSSetConstantBuffers(0, 1, m_pComCBufferPerObject->GetAdressOfBuffer());
+		pContext->PSSetConstantBuffers(0, 1, m_pComCBufferPerObject->GetAdressOfBuffer());
+	}
+
+	pContext->IASetInputLayout(m_pResVertexShader->GetInputLayout().Get());
+	pContext->VSSetShader(m_pResVertexShader->GetVertexShader().Get(), nullptr, 0);
+	pContext->PSSetShader(m_pResPixelShader->GetPixelShader().Get(), nullptr, 0);
+
+	auto pModel = m_pComModelInstance->GetModel();
+	const uint32_t iNumMeshes = pModel->Get_NumMeshes();
+	for (uint32_t i = 0; i < iNumMeshes; ++i)
+	{
+		const auto& viBuffer = pModel->GetMeshes()[i];
+
+		ID3D11Buffer* vertexBuffers[] = { viBuffer->GetVertexBuffer().Get() };
+		uint32_t strides[] = { viBuffer->GetVertexStride() };
+		uint32_t offsets[] = { 0 };
+		pContext->IASetVertexBuffers(0, 1, vertexBuffers, strides, offsets);
+		pContext->IASetIndexBuffer(viBuffer->GetIndexBuffer().Get(), viBuffer->GetIndexFormat(), 0);
+		pContext->IASetPrimitiveTopology(viBuffer->GetPrimitiveType());
+
+		m_pComModelInstance->Bind_Materials(pContext, i, AI_TEXTURE_TYPE::aiTextureType_DIFFUSE, 0);
+		pContext->PSSetSamplers(0, 1, m_pResSamplerState->GetSamplerState().GetAddressOf());
+
+		const auto& rasterizer = CGameInstance::GetConst().GetResourceFirst<CResRasterizerState>(TAG_RES_GRP_PERMANENT_STATE, TAG_RES_STATE_RS_SOLID_NOCULL);
+		if (rasterizer)
+		{
+			pContext->RSSetState(rasterizer->GetRasterizerState().Get());
+		}
+
+		pContext->DrawIndexed(viBuffer->GetNumIndices(), 0, 0);
+	}
+
+	return S_OK;
+}
+
+void CMapMeshObject::UpdateGUI()
+{
+	CGameObject::UpdateGUI();
+	ImGui::Text("Model: %s / %s", m_modelResourceGroup.c_str(), m_modelResourceTag.c_str());
+}
+
+UPtr<CMapMeshObject> CMapMeshObject::Create()
+{
+	auto pInstance = ToUPtr(new CMapMeshObject{});
+	if (FAILED(pInstance->InitializePrototype()))
+	{
+		MSG_BOX("Failed to Created : CMapMeshObject");
+		return nullptr;
+	}
+	return pInstance;
+}
+
+UPtr<CPrototype> CMapMeshObject::Clone(void* pArg)
+{
+	auto pInstance = ToUPtr(new CMapMeshObject{ *this });
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX("Failed to Cloned : CMapMeshObject");
+		return nullptr;
+	}
+
+	return pInstance;
+}
---
+++ Engine/Public/MapMeshObject.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+#include "GameObject.h"
+
+NS_BEGIN(Engine)
+
+class CComConstantBuffer;
+class CComModelInstance;
+class CResPixelShader;
+class CResSamplerState;
+class CResVertexShader;
+
+class ENGINE_DLL CMapMeshObject : public CGameObject
+{
+public:
+	DECLARE_DERIVED_TYPE(CMapMeshObject, CGameObject)
+	CMapMeshObject& operator=(const CMapMeshObject&) = delete;
+
+protected:
+	explicit CMapMeshObject();
+	explicit CMapMeshObject(const CMapMeshObject& Prototype);
+	~CMapMeshObject() override;
+
+public:
+	typedef struct tagMapMeshObjectDesc : public GAMEOBJECT_DESC
+	{
+		std::string modelGroupTag;
+		std::string modelResTag;
+		std::string protoGroupTag;
+		std::string prototypeTag;
+	} MAP_MESH_OBJECT_DESC;
+
+public:
+	HRESULT InitializePrototype(void* pArg = nullptr) override;
+	HRESULT Initialize(void* pArg) override;
+	void LateUpdate(_float fTimeDelta) override;
+	HRESULT Render(ID3D11DeviceContext* pContext, const RENDER_CTX& ctx) override;
+	void UpdateGUI() override;
+
+public:
+	const std::string& GetModelResourceGroup() const { return m_modelResourceGroup; }
+	const std::string& GetModelResourceTag() const { return m_modelResourceTag; }
+
+private:
+	std::string m_modelResourceGroup{};
+	std::string m_modelResourceTag{};
+	CComModelInstance* m_pComModelInstance{};
+	CComConstantBuffer* m_pComCBufferPerObject{};
+	SPtr<CResVertexShader> m_pResVertexShader{};
+	SPtr<CResPixelShader> m_pResPixelShader{};
+	SPtr<CResSamplerState> m_pResSamplerState{};
+
+public:
+	static UPtr<CMapMeshObject> Create();
+	UPtr<CPrototype> Clone(void* pArg) override;
+};
+
+NS_END
