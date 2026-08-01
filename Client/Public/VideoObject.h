75aa25c5be35def10c8260ef380cf6d27f65a6a8
EnoughKK <ksu8063@naver.com>
2026-08-01 21:27:57 +0900

    [feat] 영상 로드 워커스래드로 #145

--- Client/Public/VideoObject.h
+++ Client/Public/VideoObject.h
@@ -7,6 +7,12 @@
 #include <mfidl.h>
 #include <mfreadwrite.h>
 
+#include <atomic>
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <vector>
+
 #pragma comment(lib, "mfreadwrite.lib")
 #pragma comment(lib, "mfplat.lib")
 #pragma comment(lib, "mfuuid.lib")
@@ -36,30 +42,59 @@ public:
 	HRESULT Render(ID3D11DeviceContext* pContext, const E::RENDER_CTX& ctx) override;
 
 private:
-	virtual void PlayEffect(uint32_t uiState);
+	void PlayEffect(uint32_t uiState) override;
+
+private:
+	struct VIDEO_FRAME
+	{
+		std::vector<BYTE> Pixels{};
+		UINT Width{};
+		UINT Height{};
+		UINT RowPitch{};
+		LONGLONG TimeStamp{};
+	};
+
+	struct VIDEO_ASYNC_STATE
+	{
+		std::mutex Mutex{};
+		std::condition_variable Condition{};
+		std::deque<VIDEO_FRAME> Frames{};
+		std::atomic_bool Cancel{ false };
+		std::atomic_bool Failed{ false };
+	};
+
+	enum class VIDEO_LOAD_STATE
+	{
+		IDLE,
+		LOADING,
+		READY,
+		FAILED
+	};
+
+	void BeginAsyncVideoLoad();
+	void ProcessDecodedFrame();
+	HRESULT CreateVideoResources(UINT width, UINT height);
+	HRESULT UploadFrameToTexture(const VIDEO_FRAME& frame);
 
 private:
 	CComConstantBuffer* m_pComCBufferPerUI = nullptr;
 
-private:
-	// Media Foundation 객체
-	ComPtr<IMFSourceReader> m_pSourceReader;
-
-	// DX11 동적 텍스처 및 리소스 뷰
 	ComPtr<ID3D11Texture2D> m_pVideoTexture;
 	ComPtr<ID3D11ShaderResourceView> m_pVideoSRV;
 
+	std::shared_ptr<VIDEO_ASYNC_STATE> m_pAsyncState{};
+	VIDEO_LOAD_STATE m_eLoadState{ VIDEO_LOAD_STATE::IDLE };
+
 	UINT m_iVideoWidth = 0;
 	UINT m_iVideoHeight = 0;
-	float m_fTimeAcc = 0.f;     // 누적 시간
-	float m_fFrameRate = 30.f;  // 영상 프레임 레이트 (예: 30fps
-	float m_fStartDelay = 1.0f;
+	float m_fTimeAcc = 0.f;
+	float m_fFrameRate = 30.f;
+	float m_fStartDelay = 1.f;
 
 	std::wstring m_path = L"./Resources/SampleClient/Textures/UI/Video/FMV_Accio.avi";
 
-	_bool SourceReader{ false };
 public:
-	void SetPath(std::wstring videoPath) { m_path = videoPath; }
+	void SetPath(std::wstring videoPath) { m_path = std::move(videoPath); }
 
 public:
 	static E::UPtr<CVideoObject> Create();
