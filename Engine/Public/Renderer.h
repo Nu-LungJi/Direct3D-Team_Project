05676c7da77e0771d5c4fd8d141ff73e348f15c3
LungJi <shinhwa30724@gmail.com>
2026-08-04 01:19:08 +0900

    [Merge] 쉐이더 효과 ON 디폴트

--- Engine/Public/Renderer.h
+++ Engine/Public/Renderer.h
@@ -267,9 +267,9 @@ private:
 	HRESULT RenderUI();
 
 private:
-	_bool			ApplyFilter		= { false };		// 필터 적용 ON-OFF
-	_bool			ApplyVolumetric = { false };		// 볼류메트릭 효과 ON-OFF
-	_bool			ApplyShadow		= { false };		// 그림자 ON-OFF
+	_bool			ApplyFilter		= { true };		// 필터 적용 ON-OFF
+	_bool			ApplyVolumetric = { true };		// 볼류메트릭 효과 ON-OFF
+	_bool			ApplyShadow		= { true };		// 그림자 ON-OFF
 
 	RENDER_CTX		RenderContext = {};
 	XMMATRIX		ShadowLightVP{};
