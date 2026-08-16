c3c850eb32cbfa63137cec74f6aad4c5224665ba
LungJi <shinhwa30724@gmail.com>
2026-08-16 23:56:50 +0900

    [Feat] Ranrok 메쉬FX, 색감조정

--- Client/Public/EnderDragon.h
+++ Client/Public/EnderDragon.h
@@ -19,17 +19,6 @@ typedef struct stractiveskilltable
 	DRAGON_SKILL eType{};
 }EDG_ACSKT_DESC;
 
-/*----------- 광윤 추가 -----------*/
-typedef struct dragonfxmaterial
-{
-	_float4		vCoreColor;
-	_float4		vEdgeColor;
-	_float4		vAnimationParams;
-	_float4		vSurfaceParams;
-
-}DRAGON_FX_MATERIAL;
-/*---------------------------------*/
-
 NS_BEGIN(Client)
 typedef struct stredgskillInfo
 {
@@ -72,19 +61,37 @@ public:
 	void						Ready_BBKeyValue();
 
 	/*----------- 광윤 추가 -----------*/ // MaskMap Test
-	HRESULT						Render_Instanced(ID3D11DeviceContext* pContext, const E::RENDER_CTX& ctx, const E::MODEL_INSTANCE_BATCH& Batch);
-	HRESULT						Render_DragonFX(ID3D11DeviceContext* pContext, uint32_t iInstanceCount);
-	HRESULT						Bind_DragonFXMaterial(ID3D11DeviceContext* pContext, uint32_t iMaterialIndex);
+	HRESULT						Render(ID3D11DeviceContext* pContext, const E::RENDER_CTX& ctx) override;
+	HRESULT						Render_Instanced(ID3D11DeviceContext* pContext, const E::RENDER_CTX& ctx, const E::MODEL_INSTANCE_BATCH& Batch) override;
+
 private:
-	SPtr<CResPixelShader>	m_pResDragonPixelShader{};
-	SPtr<CResCBuffer>		m_pResDragonCBuffer{};
-	SPtr<CResCBuffer>		m_pResDragonFXCBuffer{};
+	HRESULT						Render_WingFXForward(ID3D11DeviceContext* pContext, const E::MODEL_INSTANCE_BATCH& Batch);
+	HRESULT						Prepare_DragonInstancing(ID3D11DeviceContext* pContext, const E::MODEL_INSTANCE_BATCH& Batch, SPtr<E::CResModel>& pOutModel, uint32_t& iOutInstanceCount);
+	HRESULT						Bind_SkinnedMeshConstantBuffer(ID3D11DeviceContext* pContext, SPtr<E::CResModel>& pModel, SPtr<CResModelMesh>& pMesh, uint32_t iMeshIndex);
+private:
+	const E::MODEL_INSTANCE_BATCH* m_pPendingWingFXBatch = nullptr;
+	_bool m_bWingFXQueued = false;
 
-	SPtr<CResModel>			m_pDragonFXModel;
-	SPtr<CResStaticModel>	m_pDragonSpineModel;
+private:
+	SPtr<CResTexture2D>		m_pBodyMaskTexture{};
+	SPtr<CResTexture2D>		m_pWingsMaskTexture{};
 
-	SPtr<CResPixelShader>	m_pDragonFXPixelShader{};
-	SPtr<CResPixelShader>	m_pDragonSpinePixelShader{};
+	SPtr<CResTexture2D>		m_pBodyMROTexture{};
+	SPtr<CResTexture2D>		m_pWingsMROTexture{};
+
+	SPtr<CResTexture2D>		m_pEtherealWingsTexture{};
+
+	SPtr<CResTexture2D>		m_pMarbleNoiseTexture{};
+	SPtr<CResTexture2D> 	m_pRiverNoiseTexture{};
+	SPtr<CResTexture2D> 	m_pCausticNoiseTexture{};
+	SPtr<CResTexture2D> 	m_pDetailNoiseTexture{};
+
+	SPtr<CResPixelShader>	m_pResDragonBodyPixelShader{};
+	SPtr<CResPixelShader>	m_pResDragonWingPixelShader{};
+	SPtr<CResPixelShader>	m_pResDragonWingFXPixelShader{};
+
+	SPtr<CResDepthStencilState>	m_pResWingFXDSS{};
+	SPtr<CResRasterizerState>	m_pResWingFXRasterizer{};
 	/*---------------------------------*/
 
 public:
